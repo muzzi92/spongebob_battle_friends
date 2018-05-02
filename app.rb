d80600b57018be9388be307e1015075c44a96bbc
Alastair Edmonds <Alastair.Edmonds@gmail.com>
2018-05-02 15:04:30 +0100

    third first commit with Asia

--- app.rb
+++ app.rb
@@ -3,17 +3,7 @@ require 'sinatra/base'
 class Battle < Sinatra::Base
 
 get '/' do
-<<<<<<< HEAD
   erb(:index)
-=======
-  erb :index
-end
-
-post '/names' do
-  @player_1_name = params[:player_1_name]
-  @player_2_name = params[:player_2_name]
-  erb :play
->>>>>>> 5a75334632d8c24ccd29aea304c5dc22de7d3743
 end
 
 post '/names' do
@@ -21,7 +11,6 @@ post '/names' do
   @player_2 = params[:Player_2]
   erb(:play)
 end
-
 
 run! if app_file == $0
 
