dcb1a0cb5ea48dcf6d2caaf7e4fd181078bff604
Alastair Edmonds <Alastair.Edmonds@gmail.com>
2018-05-02 15:54:37 +0100

    Add session & redirect

--- spec/features_spec.rb
+++ spec/features_spec.rb
@@ -2,9 +2,10 @@ feature 'battle' do
 
 scenario 'Add player names' do
   visit('/')
-  fill_in('Player_1', with: 'Asia')
-  fill_in('Player_2', with: 'Alastair')
+  save_and_open_page
+  fill_in('player_1', with: 'Asia')
+  fill_in('player_2', with: 'Alastair')
   click_button 'Submit'
-  expect(page).to have_content ('Asia Vs. Alastair')
+  expect(page).to have_content ("Asia Vs. Alastair")
   end
 end
