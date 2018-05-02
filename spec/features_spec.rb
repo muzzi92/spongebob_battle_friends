71ef012359a90dceb3391b77c11306a6c1b384eb
Asia Antczak <aasiaantczak@gmail.com>
2018-05-02 16:21:51 +0100

    add sign_in_and_play method to keep tests DRY

--- spec/features_spec.rb
+++ spec/features_spec.rb
@@ -1,19 +1,13 @@
 feature 'battle' do
   scenario 'Add player names' do
-    visit('/')
-    fill_in('player_1', with: 'Asia')
-    fill_in('player_2', with: 'Alastair')
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content("Asia Vs. Alastair")
   end
 end
 
 feature 'view hit hitpoints' do
   scenario 'See players 2 hit points' do
-    visit('/')
-    fill_in('player_1', with: 'Asia')
-    fill_in('player_2', with: 'Alastair')
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content('Alastair: 100 HP')
   end
 
