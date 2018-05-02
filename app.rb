1e297d1f67d76ba476a063ff7007c5e45f5a2974
Alastair Edmonds <Alastair.Edmonds@gmail.com>
2018-05-02 16:49:25 +0100

    Add Attack

--- app.rb
+++ app.rb
@@ -20,7 +20,11 @@ get '/play' do
   erb(:play)
 end
 
-
+get '/attack' do
+  @player_1 = session[:player_1]
+  @player_2 = session[:player_2]
+  erb(:attack)
+end
 
 run! if app_file == $0
 
--- spec/features_spec.rb
+++ spec/features_spec.rb
@@ -1,14 +1,21 @@
-feature 'battle' do
+feature 'Battle' do
   scenario 'Add player names' do
     sign_in_and_play
     expect(page).to have_content("Asia Vs. Alastair")
   end
 end
 
-feature 'view hit hitpoints' do
+feature 'View hitpoints' do
   scenario 'See players 2 hit points' do
     sign_in_and_play
     expect(page).to have_content('Alastair: 100 HP')
   end
 
+feature 'Attack' do
+  scenario 'Attack player_2 & see confirmation' do
+    sign_in_and_play
+    click_link 'Attack'
+    expect(page).to have_content("Alastair has been hit")
+    end
+  end
 end
